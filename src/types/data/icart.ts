a48f55cc520678b34020adf0e0148d86e0829ea0
Yaroslav Privalov <334633@niuitmo.ru>
2025-12-23 17:55:38 +0300

    fixed spec

--- src/types/data/icart.ts
+++ src/types/data/icart.ts
@@ -3,4 +3,7 @@ import { IProduct } from './iproduct';
 export interface ICart {
 	id: string;
 	items: IProduct[];
+
+	putInCart(productId: string): void;
+	deleteFromCart(productId: string): void;
 }
--- src/types/services/IcartService.ts
+++
@@ -1,7 +0,0 @@
-import { ICart } from '../data/icart';
-
-export interface ICartService {
-	getCart(id: string): Promise<ICart>;
-	putInCart(cartId: string, productId: string): Promise<ICart>;
-	deleteFromCart(cartId: string, productId: string): Promise<void>;
-}
